70991d203fe53ad7bf62d2544a57e548d09805f8
sachinraghult <sachinraghult2002@gmail.com>
2022-08-11 23:53:15 +0530

    ADD: password reset by sending email

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,11 +1,12 @@
-from flaskblog import app, db, bcrypt
+from flaskblog import app, db, bcrypt, mail
 from flask import render_template, url_for, flash, redirect, request, abort
-from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
+from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm, RequestResetForm, ResetPasswordForm
 from flaskblog.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
+from flask_mail import Message
+from PIL import Image
 import secrets
 import os
-from PIL import Image
 
 
 @app.route("/")
@@ -166,3 +167,55 @@ def user_posts(username):
         .order_by(Post.date_posted.desc())\
         .paginate(page=page, per_page=5)
     return render_template('user_posts.html', posts=posts, user=user)
+
+
+def send_reset_email(user):
+    token = user.get_reset_token()
+    msg = Message('Password Reset Request',
+                  sender='noreply@demo.com', recipients=[user.email])
+    msg.body = f'''To reset your password, visit the following link:
+{url_for('reset_token', token=token, _external=True)}
+
+If you did not make this request then simply ignore this email and no changes will be made.
+'''
+    try:
+        mail.send(msg)
+        return True
+    except:
+        return False
+
+
+@ app.route("/reset_password", methods=['GET', 'POST'])
+def reset_request():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = RequestResetForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if send_reset_email(user):
+            flash('An email has been sent to reset your password.', 'info')
+            return redirect(url_for('login'))
+        else:
+            flash(
+                'An error occured in sending email as it cannot be sent from less secured apps', 'danger')
+            return redirect(url_for('reset_request'))
+    return render_template('reset_request.html', title='Reset Password', form=form)
+
+
+@ app.route("/reset_password/<token>", methods=['GET', 'POST'])
+def reset_token():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    user = User.verify_reset_token(token)
+    if user is None:
+        flash('That is an invalid or expired token', 'warning')
+        return redirect(url_for('reset_request'))
+    form = ResetPasswordForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(
+            form.password.data).decode('utf-8')
+        user.password = hashed_password
+        db.session.commit()
+        flash('Your password has been updated! You are now able to log in', 'success')
+        return redirect(url_for('login'))
+    return render_template('reset_token.html', title='Reset Password', form=form)
