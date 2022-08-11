a324adcd53f79a5887616173c3c398ad6d9fc0d5
sachinraghult <sachinraghult2002@gmail.com>
2022-08-12 00:57:29 +0530

    ADD: restructuring into blueprints

---
+++ flaskblog/config.py
@@ -0,0 +1,11 @@
+import os
+
+
+class Config:
+    SECRET_KEY = '6da3ca80fa08cb3de5539d41b0201e69'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('EMAIL_USER')
+    MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
